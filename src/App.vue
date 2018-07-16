c4ed69dc480829c255d894656575c5afddee2955
g905 <breakestra@yandex.ru>
2018-07-16 20:08:14 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <eheader></eheader>
+  </div>
+</template>
+
+<script>
+import eheader from './components/eheader'
+export default {
+  components: {
+    eheader
+  },
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import "./assets/sass/main.sass"
+</style>
---
+++ src/components/eheader.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div id="eheader" class="container">
+  <header class="header">
+    <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light" role="navigation">
+      <a class="navbar-brand" href="#" role="banner"><img src="../assets/img/logo_fullsize.svg" alt="Электрон-лого"></a>
+    </nav>
+  </header>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="sass">
+</style>
